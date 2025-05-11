773277e734fa9be6ab76f8c3fa5401eb771dad30
Davit Aleksanyan <davitaleksanyan@Davits-MacBook-Air.local>
2025-05-11 13:47:26 +0400

    Updated the single number solution

--- singleNumber.cpp
+++ singleNumber.cpp
@@ -1,3 +1,8 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
@@ -8,3 +13,17 @@ public:
         return res;
     }
 };
+
+int main() {
+    Solution sol;
+    
+    int arr[] = {4, 1, 2, 1, 2};
+    vector<int> nums(arr, arr + sizeof(arr)/sizeof(arr[0]));
+
+    int result = sol.singleNumber(nums);
+
+    cout << "The single number is: " << result << endl;
+
+    return 0;
+}
+
